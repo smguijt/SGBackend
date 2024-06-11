80c8aa2285abd45381bee8c28eeee96e03a6cdd9
Sietse Guijt <smguijt@outlook.com>
2024-06-11 23:16:02 +0200

    add template for Unify Admin Free bootstrap 5 template

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -5,8 +5,8 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) async throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    // serve files from /Public folder
+    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite)
 
