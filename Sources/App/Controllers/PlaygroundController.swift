0fb6f2243f0ee69a3a1e0cff1c6279e98ba1038d
Sietse Guijt <smguijt@outlook.com>
2024-06-13 22:39:11 +0200

    update template

--- Sources/App/Controllers/PlaygroundController.swift
+++ Sources/App/Controllers/PlaygroundController.swift
@@ -7,11 +7,18 @@ struct PlaygroundController: RouteCollection {
         let pg = routes.grouped("playground")
         
         pg.get(use: self.index)
+        pg.get("item1", use: self.renderItem1)
     }
     
     @Sendable
     func index(req: Request) async throws -> View {
         req.logger.info("calling playground.index")
-        return try await req.view.render("playground/index")
+        return try await req.view.render("playgroundIndex")
+    }
+    
+    @Sendable
+    func renderItem1(req: Request) async throws -> View {
+        req.logger.info("calling playground.item1")
+        return try await req.view.render("playgroundItem1")
     }
 }
