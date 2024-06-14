9694f95f3b797488557f8c8309336aad95ad1464
Sietse Guijt <smguijt@outlook.com>
2024-06-14 16:20:52 +0200

    .

--- Sources/App/Controllers/PlaygroundController.swift
+++ Sources/App/Controllers/PlaygroundController.swift
@@ -9,6 +9,9 @@ struct PlaygroundController: RouteCollection {
         
         pg.get(use: self.index)
         pg.get("item1", use: self.renderItem1)
+        pg.get("systemsettings", use: self.renderSystemSettings)
+        pg.post("systemsettings", use: self.updateSetting)
+        
     }
     
     @Sendable
@@ -24,46 +27,20 @@ struct PlaygroundController: RouteCollection {
         return try await req.view.render("playgroundItem1")
     }
     
-    private func getSettings(db: Database) async throws -> SettingsDTO {
-        /* retrieve settings */
-        var mySettingDTO: SettingsDTO = SettingsDTO()
-        _ = try await Setting.query(on: db).all().compactMap{ setting in
-            print("setting: \(setting)")
-            
-            /* ShowToolbar*/
-            if setting.key == eSettings.ShowToolbar.rawValue {
-                print("value: \(String(describing: setting.value.lowercased()))")
-                mySettingDTO.ShowToolbar = Bool(setting.value.lowercased()) ?? false
-            }
-            
-            /* ShowMessages*/
-            if setting.key == eSettings.ShowMessages.rawValue {
-                print("value: \(String(describing: setting.value.lowercased()))")
-                mySettingDTO.ShowMessages = Bool(setting.value.lowercased()) ?? false
-            }
-            
-            /* ShowApps*/
-            if setting.key == eSettings.ShowApps.rawValue {
-                print("value: \(String(describing: setting.value.lowercased()))")
-                mySettingDTO.ShowApps = Bool(setting.value.lowercased()) ?? false
-            }
-            
-            /* ShowNotifications*/
-            if setting.key == eSettings.ShowToolbar.rawValue {
-                print("value: \(String(describing: setting.value.lowercased()))")
-                mySettingDTO.ShowToolbar = Bool(setting.value.lowercased()) ?? false
-            }
-            
-            /* ShowUpdates*/
-            if setting.key == eSettings.ShowUpdates.rawValue {
-                print("value: \(String(describing: setting.value.lowercased()))")
-                mySettingDTO.ShowUpdates = Bool(setting.value.lowercased()) ?? false
-            }
-            
-            return setting
-        }
-        return mySettingDTO
+    @Sendable
+    func renderSystemSettings(req: Request) async throws -> View {
+        req.logger.info("calling playground.systemsettings")
+        let mySettingsDTO = try await getSettings(db: req.db)
+        return try await req.view.render("playgroundSystemSettings", BaseContext(title: "Playground", settings: mySettingsDTO))
     }
     
+    @Sendable
+    func updateSetting(_ req: Request) throws -> HTTPStatus {
+        req.logger.info("calling playground.systemsettings POST")
+        
+        print(req.body)
+        
+        return HTTPStatus.accepted
+    }
    
 }
--- Sources/App/Shared/SharedLib.swift
+++ Sources/App/Shared/SharedLib.swift
@@ -2,12 +2,44 @@ import Foundation
 import Fluent
 import Vapor
 
-enum SGSharedLib {
-    struct v1: RawRepresentable {
-        var rawValue: String
-    }
-}
 
-extension SGSharedLib.v1 {
-    
+ func getSettings(db: Database) async throws -> SettingsDTO {
+    /* retrieve settings */
+    var mySettingDTO: SettingsDTO = SettingsDTO()
+    _ = try await Setting.query(on: db).all().compactMap{ setting in
+        print("setting: \(setting)")
+        
+        /* ShowToolbar*/
+        if setting.key == eSettings.ShowToolbar.rawValue {
+            print("value: \(String(describing: setting.value.lowercased()))")
+            mySettingDTO.ShowToolbar = Bool(setting.value.lowercased()) ?? false
+        }
+        
+        /* ShowMessages*/
+        if setting.key == eSettings.ShowMessages.rawValue {
+            print("value: \(String(describing: setting.value.lowercased()))")
+            mySettingDTO.ShowMessages = Bool(setting.value.lowercased()) ?? false
+        }
+        
+        /* ShowApps*/
+        if setting.key == eSettings.ShowApps.rawValue {
+            print("value: \(String(describing: setting.value.lowercased()))")
+            mySettingDTO.ShowApps = Bool(setting.value.lowercased()) ?? false
+        }
+        
+        /* ShowNotifications*/
+        if setting.key == eSettings.ShowNotifications.rawValue {
+            print("value: \(String(describing: setting.value.lowercased()))")
+            mySettingDTO.ShowNotifications = Bool(setting.value.lowercased()) ?? false
+        }
+        
+        /* ShowUpdates*/
+        if setting.key == eSettings.ShowUpdates.rawValue {
+            print("value: \(String(describing: setting.value.lowercased()))")
+            mySettingDTO.ShowUpdates = Bool(setting.value.lowercased()) ?? false
+        }
+        
+        return setting
+    }
+    return mySettingDTO
 }
