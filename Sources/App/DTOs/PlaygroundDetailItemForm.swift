b2825ba01fec9cadbbc200b7daac461dc71d87be
Sietse Guijt <smguijt@outlook.com>
2024-06-24 18:06:02 +0200

    .

--- Sources/App/DTOs/PlaygroundDetailItemForm.swift
+++ Sources/App/DTOs/PlaygroundDetailItemForm.swift
@@ -5,12 +5,40 @@ struct PlaygroundDetailItemForm: Content {
     
     let paramId: String?
     let text_1: String?
+    let text_2: String?
+    let text_3: String?
+    let text_4: String?
+    let text_5: String?
     let date_1: String?
+    let date_2: String?
+    let date_3: String?
+    let date_4: String?
+    let date_5: String?
+    let int_1: String?
+    let int_2: String?
+    let int_3: String?
+    let int_4: String?
+    let int_5: String?
     
-    init(paramId: String?, text_1: String?, date_1: String?) {
+    init(paramId: String?, text_1: String?, text_2: String?, text_3: String?, text_4: String?, text_5: String?, date_1: String?, date_2: String?, date_3: String?, date_4: String?, date_5: String?, int_1: String?, int_2: String?, int_3: String?, int_4: String?, int_5: String?) {
         self.paramId = paramId
         self.text_1 = text_1
+        self.text_2 = text_2
+        self.text_3 = text_3
+        self.text_4 = text_4
+        self.text_5 = text_5
         self.date_1 = date_1
+        self.date_2 = date_2
+        self.date_3 = date_3
+        self.date_4 = date_4
+        self.date_5 = date_5
+        self.int_1 = int_1
+        self.int_2 = int_2
+        self.int_3 = int_3
+        self.int_4 = int_4
+        self.int_5 = int_5
     }
+    
+    
 }
 
--- Sources/App/Shared/SharedLib.swift
+++ Sources/App/Shared/SharedLib.swift
@@ -15,47 +15,48 @@ import Vapor
     /* retrieve settings */
     var mySettingDTO: SettingsDTO = SettingsDTO()
     _ = try await Setting.query(on: req.db).all().compactMap{ setting in
-        print("setting: \(setting)")
+        //req.logger.info("setting: \(setting)")
+        
         
         /* ShowToolbar*/
         if setting.key == eSettings.ShowToolbar.rawValue {
-            print("value: \(String(describing: setting.value.lowercased()))")
+            //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
             mySettingDTO.ShowToolbar = Bool(setting.value.lowercased()) ?? false
         }
         
         /* ShowMessages*/
         if setting.key == eSettings.ShowMessages.rawValue {
-            print("value: \(String(describing: setting.value.lowercased()))")
+            //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
             mySettingDTO.ShowMessages = Bool(setting.value.lowercased()) ?? false
         }
         
         /* ShowApps*/
         if setting.key == eSettings.ShowApps.rawValue {
-            print("value: \(String(describing: setting.value.lowercased()))")
+            //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
             mySettingDTO.ShowApps = Bool(setting.value.lowercased()) ?? false
         }
         
         /* ShowNotifications*/
         if setting.key == eSettings.ShowNotifications.rawValue {
-            print("value: \(String(describing: setting.value.lowercased()))")
+            //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
             mySettingDTO.ShowNotifications = Bool(setting.value.lowercased()) ?? false
         }
         
         /* ShowUpdates*/
         if setting.key == eSettings.ShowUpdates.rawValue {
-            print("value: \(String(describing: setting.value.lowercased()))")
+            //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
             mySettingDTO.ShowUpdates = Bool(setting.value.lowercased()) ?? false
         }
 
         /* ShowUserBox*/
         if setting.key == eSettings.ShowUserBox.rawValue {
-            print("value: \(String(describing: setting.value.lowercased()))")
+            //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
             mySettingDTO.ShowUserBox = Bool(setting.value.lowercased()) ?? false
         }
         
         /* ShowUseOAUTH02 */
         if setting.key == eSettings.UseOAUTH02.rawValue {
-            req.logger.info("value: \(String(describing: setting.value.lowercased()))")
+            //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
             mySettingDTO.UseOAUTH02 = Bool(setting.value.lowercased()) ?? false
         }
     }
@@ -63,7 +64,7 @@ import Vapor
      // load userId from session
     mySettingDTO.userId = UUID(req.session.data["sgsoftware_systemuser"] ?? "")
 
-    req.logger.info("geSystemSettings data parsed: \(mySettingDTO)")
+    //req.logger.info("geSystemSettings data parsed: \(mySettingDTO)")
     return mySettingDTO
 }
 
@@ -71,62 +72,60 @@ import Vapor
      
     var myUserSettingDTO: UserSettingsDTO = UserSettingsDTO(userId: userId)
 
-     let _tmpSettings = try await UserSettings
+     _ = try await UserSettings
             .query(on: req.db)
             .filter(\.$userId == userId)
             .all()
             .compactMap { setting in
-                req.logger.info("userSetting: \(setting)")  
+                //req.logger.info("userSetting: \(setting)")
 
                 /* ShowMessages*/
                 if setting.key == eSettings.ShowMessages.rawValue {
-                    req.logger.info("value: \(String(describing: setting.value.lowercased()))")
+                    //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
                     myUserSettingDTO.ShowMessages = Bool(setting.value.lowercased()) ?? false
                 }  
 
                 /* ShowApps*/
                 if setting.key == eSettings.ShowApps.rawValue {
-                    req.logger.info("value: \(String(describing: setting.value.lowercased()))")
+                    //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
                     myUserSettingDTO.ShowApps = Bool(setting.value.lowercased()) ?? false
                 }
                 
                 /* ShowNotifications*/
                 if setting.key == eSettings.ShowNotifications.rawValue {
-                    req.logger.info("value: \(String(describing: setting.value.lowercased()))")
+                    //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
                     myUserSettingDTO.ShowNotifications = Bool(setting.value.lowercased()) ?? false
                 }
                 
                 /* ShowUpdates*/
                 if setting.key == eSettings.ShowUpdates.rawValue {
-                    req.logger.info("value: \(String(describing: setting.value.lowercased()))")
+                    //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
                     myUserSettingDTO.ShowUpdates = Bool(setting.value.lowercased()) ?? false
                 }
                 
                 /* ShowUseOAUTH02 */
                 if setting.key == eSettings.UseOAUTH02.rawValue {
-                    req.logger.info("value: \(String(describing: setting.value.lowercased()))")
+                    //req.logger.info("value: \(String(describing: setting.value.lowercased()))")
                     myUserSettingDTO.UseOAUTH02 = Bool(setting.value.lowercased()) ?? false
                 }
                 
                 /* ClientId */
                 if setting.key == eSettings.ClientId.rawValue {
-                    req.logger.info("value: \(String(describing: setting.value))")
+                    //req.logger.info("value: \(String(describing: setting.value))")
                     myUserSettingDTO.ClientId = String(setting.value)
                 }
                 
                 /* ClientSecret */
                 if setting.key == eSettings.ClientSecret.rawValue {
-                    req.logger.info("value: \(String(describing: setting.value))")
+                    //req.logger.info("value: \(String(describing: setting.value))")
                     myUserSettingDTO.ClientSecret = String(setting.value)
                 }
 
                 return myUserSettingDTO
             }
 
-     req.logger.info("geUserSettings data parsed: \(_tmpSettings)")
-     
-    //req.logger.info("geUserSettings data parsed: \(myUserSettingDTO)")
-    req.logger.info("load system settings")
+    //req.logger.info("geUserSettings data parsed: \(_tmpSettings)")
+    //req.logger.info("load system settings")
     let mySystemSettings: SettingsDTO = try await getSystemSettings(req: req)
     
     //req.logger.info("update settings output")
